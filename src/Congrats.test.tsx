a04a6fcd1d9ade26435612dd85d6e133383b4bc4
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-03-12 20:19:03 +0200

    Test if Congrats component renders without errors

--- src/Congrats.test.tsx
+++ src/Congrats.test.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import Enzyme, { shallow, ShallowWrapper } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import Congrats, { CongratsProps } from './Congrats';
+import { findByTestAttr } from "../test/testUtils";
 
 Enzyme.configure({
     adapter: new EnzymeAdapter()
 });
 
-const setup = (props: CongratsProps): ShallowWrapper => {
+const setup = (props: CongratsProps = {}): ShallowWrapper => {
     return shallow(<Congrats { ...props } />)
 }
 
 test("renders without error", () => {
-
+    const wrapper = setup();
+    const congratsComponent = findByTestAttr(wrapper, "component-congrats");
+    expect(congratsComponent).toHaveLength(1);
 });
 
 test("renders no text when `success` prop is false", () => {
@@ -22,3 +25,4 @@ test("renders no text when `success` prop is false", () => {
 test("renders non-empty congrats message when `success` prop is true", () => {
 
 });
+
