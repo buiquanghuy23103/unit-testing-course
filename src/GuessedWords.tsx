28918c4f67cb99ad281457c9b55c5dd48c7822a7
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-03-13 14:09:59 +0200

    Code for nonzero guessed words

--- src/GuessedWords.tsx
+++ src/GuessedWords.tsx
@@ -10,9 +10,44 @@ export type GuessedWordsProps = {
 }
 
 export default function GuessedWords(props: GuessedWordsProps) {
+    let content: JSX.Element;
+
+    const random = () => {
+        return Math.random().toString(16).substr(2, 8);
+    }
+
+    const guessWordsRows = props.guessedWords.map(guessedWord => {
+        return (
+            <tr key={ random() } data-test="guessed-word">
+                <td>{ guessedWord.word }</td>
+                <td>{ guessedWord.letterMatchCount }</td>
+            </tr>
+        );
+    });
+
+    if (props.guessedWords.length === 0) {
+        content = (<h4 data-test="instruction">Try to guess the secret word</h4>);
+    } else {
+        content = (
+            <div data-test="guessed-words-section">
+                <h3>Guessed words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching letters</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { guessWordsRows }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
     return (
         <div data-test="component-guessed-words">
-            <h4 data-test="instruction">Try to guess the secret word</h4>
+            {content }
         </div>
     )
 }
