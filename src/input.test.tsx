e16fdc4135c64fb68f652ecb754d106f1f12a434
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-03-25 15:24:51 +0200

    Write test for input change event

--- src/input.test.tsx
+++ src/input.test.tsx
@@ -1,4 +1,5 @@
 import { shallow } from "enzyme";
+import React from "react";
 import { findByTestAttr } from "../test/testUtils";
 import Input from "./input";
 
@@ -11,3 +12,21 @@ test('should render without errors', () => {
     const component = findByTestAttr(wrapper, "component-input");
     expect(component).toHaveLength(1);
 });
+
+describe('state controlled input field', () => {
+    test('should update state upon input changes', () => {
+        const mockSetCurrentGuess = jest.fn();
+        // Mock useState() method
+        React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+        const wrapper = setup();
+        const inputBox = findByTestAttr(wrapper, "input-box");
+
+        const mockEvent = { target: { value: "train" } };
+        inputBox.simulate("change", mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+    });
+
+})
+
