ef26cbeda4ed72a02f36c75f15b94db7cd8e3db7
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-03-30 15:23:44 +0300

    Setup Redux

--- src/actions/__mocks__/index.ts
+++ src/actions/__mocks__/index.ts
@@ -1,7 +1,9 @@
-module.exports = {
+const actionModule = {
     // Keep everything in the module the same as it be
     ...jest.requireActual('..'),
     // We want to use 'es' import statement
     __esModule: true,
     getSecretWord: jest.fn().mockReturnValue(Promise.resolve('party')),
-}
+};
+
+export default actionModule;
