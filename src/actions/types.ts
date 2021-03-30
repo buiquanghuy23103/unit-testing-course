4fb06ad47d7152e38178224573b042a2013da9f9
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-03-30 21:44:28 +0300

    Add successReducer and test

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,7 +1,8 @@
 export const CORRECT_GUESS = 'CORRECT_GUESS';
+export const NONE = 'NONE';
 
 interface CorrectGuessAction {
     type: typeof CORRECT_GUESS
 }
 
-export type GuessActionType = CorrectGuessAction;
+export type GuessActionType = CorrectGuessAction | None;
--- src/reducers/successReducer.ts
+++ src/reducers/successReducer.ts
@@ -1,3 +1,14 @@
-export default function successReducer() {
-    return null;
+import { CORRECT_GUESS, GuessActionType } from "../actions/types";
+
+export default function successReducer(
+    prevState: boolean = false,
+    action: GuessActionType
+): boolean {
+    switch (action.type) {
+        case CORRECT_GUESS:
+            return true;
+
+        default:
+            return prevState;
+    }
 }
