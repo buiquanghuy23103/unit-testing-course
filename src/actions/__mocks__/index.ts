c8db1de3db217e8ce2be40f660d91e9b59526dca
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-03-31 14:46:06 +0300

    Mock actions module again

--- src/actions/__mocks__/index.ts
+++ src/actions/__mocks__/index.ts
@@ -1,9 +1,7 @@
-const actionModule = {
+module.exports = {
     // Keep everything in the module the same as it be
-    ...jest.requireActual('..'),
+    ...jest.requireActual('../'),
     // We want to use 'es' import statement
     __esModule: true,
     getSecretWord: jest.fn().mockReturnValue(Promise.resolve('party')),
 };
-
-export default actionModule;
